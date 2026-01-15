e505ee932962268731526ae7ca6268c775fec8e4
Gustavo-Suporte <gustavo.cassara@gmail.com>
2026-01-15 10:38:58 -0300

    Implementação da opção dos tipos de contas

--- src/Main.java
+++ src/Main.java
@@ -5,15 +5,47 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         double dinheiro;
         int opcao = 0;
+        String conta = "";
+        int tipoConta = 0;
 
+        //inserção dos dados inciais
         System.out.println("Digite os dados do cliente:");
         System.out.print("Nome: ");
         String nome = sc.nextLine();
-        System.out.print("Tipo de conta: ");
-        String tipoConta = sc.next();
+
+        while (tipoConta != 1 && tipoConta != 2){ //seleciona um tipo de conta
+
+            System.out.print("""
+                        Selecione o tipo de Conta:
+                        1 - Corrente
+                        2 - Poupança
+                        """);
+
+            tipoConta = sc.nextInt();
+
+            if (tipoConta > 2) {
+                System.out.println("Opção Inválida!");
+            }
+
+            if (tipoConta < 1) {
+                System.out.println("Opção Inválida!");
+            }
+
+            switch (tipoConta){
+                case 1:
+                    conta = "Corrente";
+                    break;
+
+                case 2:
+                    conta = "Poupança";
+                    break;
+            }
+        }
+
         System.out.print("Saldo inicial: ");
         double saldo = sc.nextDouble();
 
+        //mostra os dados iniciais 
         System.out.printf(String.format("""
                 *******************************************
                 Dados iniciais do cliente:
@@ -23,7 +55,7 @@ public class Main {
                 Saldo inicial:  R$ %.2f
                 *******************************************
                 
-                """,nome,tipoConta,saldo));
+                """,nome,conta,saldo));
 
         while (opcao != 4) { //menu
             System.out.printf(String.format("""
